435d04f03ecbe4155361336d95ea9f0a20edce6b
CataRoman <croman1@umassd.edu>
2023-06-26 13:37:37 -0400

    add strat means by year

--- R/strat-mean/01a-nowind-loop.R
+++ R/strat-mean/01a-nowind-loop.R
@@ -1,5 +1,5 @@
 ### created: 04/27/2022
-### last updated:
+### last updated: 06/26/2023
 
 # 01a - STRATIFIED CALCULATIONS: WITH WIND AREAS INCLUDED ####
 
@@ -30,7 +30,7 @@ species <- readRDS(here(sseep.analysis, "data", "rds", "95filtered-species.rds")
 
 #
 strata_wts <- readRDS(here(sseep.analysis, "data", "rds", "strata_wts.rds"))
-
+BTSArea <- sum(strata_wts$Area_SqNm)
 
 
 # CALCULATE INDIVIDUAL MEANS AND VARIANCES ####
@@ -80,3 +80,50 @@ saveRDS(nowind_stratmu, here("data", "rds", "num-strat-mu_included.rds"))
 #   group_by(SEASON, AREA) %>%
 #   summarise(towct = length(unique(code))) #%>%
 #   #left_join(specieslookup, by = "SVSPP")
+#
+#
+##################################################
+
+#### CALCULATE INDIVIDUAL MEANS AND VARIANCES BY YEAR####
+# calculate individual means and variances for each combinations of species, year, and stratum
+nowind_means_yr <- data %>%
+  #mutate(tow_code = str_c(STRATUM, CRUISE6, STATION)) %>%
+  #left_join(geounits, by = "code") %>%
+  #rename(STRATUM = STRATUM.x,
+  #       STATION = STATION.x) %>%
+  group_by(STRATUM, EST_YEAR, SVSPP, COMNAME) %>% #, GEO_AREA) %>%
+  summarise(towct = length(unique(STATION)), # calculate unique tows
+            mu = sum(EXPCATCHNUM)/towct, # find the average biomass based on unique tows rather than observations to avoid potential duplication
+            var = ifelse(towct == 1, 0, # if the tow count equals 1, then variance about the mean should be 0
+                         sum((EXPCATCHNUM - mu)^2)/(towct - 1))) %>% # if tow count does not equal 1, then find the variance of biomass
+  left_join(strata_wts, by = "STRATUM") %>% # add each stratum area and relative weight to the dataset based on STRATUM number
+  mutate(wt_mu = Area_SqNm * mu, # part one of the stratified mean formula
+         wt_var = ((((RelWt)^2) * var) / towct) * (1 - (towct / Area_SqNm))) # part one of the stratified variance formula
+
+
+### save the data
+saveRDS(nowind_means_yr, here("data", "rds", "indiv-mu_included.rds"))
+
+
+
+#### COMPLETE STRATIFIED MEAN AND VARIANCE CALCULATIONS BY YEAR####
+# calculate stratified means and variances for each combinations of species and year based on individual stratum means and variances
+nowind_stratmu_yr <- nowind_means_yr %>%
+  group_by(SVSPP,COMNAME, EST_YEAR) %>% #, GEO_AREA) %>%
+  summarise(stratmu = (sum(wt_mu)) / BTSArea, # part two of the stratified mean formula
+            stratvar = sum(wt_var)) %>% # part two of the stratified variance formula
+  mutate(TYPE = paste("With Wind Included")) # paste identifying information of means and variances for joining and plotting in later scripts
+
+
+### save the data
+saveRDS(nowind_stratmu_yr, here("data", "rds", "strat-mu_included.rds"))
+
+
+##
+(strat_yr <- nowind_stratmu_yr |>
+    filter(SVSPP %in% c(141, 105, 106, 121,15,131,32,72,503,22,23,24,25,26,27,28, 103, 143)))
+write_csv(strat_yr, file = "strat_yr.csv")
+
+
+
+
