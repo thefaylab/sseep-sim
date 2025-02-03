601daa3a819e6722fbf1f6aaa51e8c4dd7e34924
Catalina Roman <85707988+CataRoman@users.noreply.github.com>
2025-02-03 15:17:14 -0500

    Update 04d_survey_validation_fall.R

--- R/scup/04d_survey_validation_fall.R
+++ R/scup/04d_survey_validation_fall.R
@@ -50,44 +50,35 @@ ggplot(obs_catch_fall) +
 
 
 # the survey grid as dataframe
-grid_xy_m2 <- readRDS(here("data", "rds", "survey_grid_all_122024.rds")) |>
+grid_xy <- readRDS(here("data", "rds", "survey_grid_all_122024.rds")) |>
   rename(strat = STRATUM,
          x = X,
          y = Y,
-         depth = median_2) |>#, # rename to match column calls within SimSurvey functions
+         depth = mean_2) |>#, # rename to match column calls within SimSurvey functions
   mutate(division = 1) |>#, # add division information
   dplyr::select(x,y,cell,depth, strat, AREA_CODE,division) |>
   data.table::as.data.table() |>
   drop_na()
 
 # as stars object
-grid_stars_2 <- readRDS(here("data",  "survey_grid_all_stars_122024.rds")) |>
+grid_stars <- readRDS(here("data",  "survey_grid_all_stars_122024.rds")) |>
   rename(depth = mean_2) |>
   dplyr::select(cell,depth, strat, AREA_CODE)
 
 
 
-summary(grid_xy_1$depth)
-summary(grid_xy_2$depth)
-summary(grid_xy_m1$depth)
-summary(grid_xy_m2$depth)
+summary(grid_xy$depth)
 
-# the survey Grid as stars object
-#grid_stars <- readRDS(here("R","scup","data",  "survey_grid_stars_062022.rds"))
-grid_stars_2
 
 # load spring model predictions
-fall_preds_1 <- readRDS(here(sseep.analysis, "sdmtmb", "scup", "data","fall_grid_preds_tw_1.rds"))
-fall_preds_2 <- readRDS(here(sseep.analysis, "sdmtmb", "scup", "data","fall_grid_preds_tw_2.rds"))
-fall_preds_m1 <- readRDS(here(sseep.analysis, "sdmtmb", "scup", "data","fall_grid_preds_tw_m1.rds"))
-fall_preds_m2 <- readRDS(here(sseep.analysis, "sdmtmb", "scup", "data","fall_grid_preds_tw_m2.rds"))
+fall_preds <- readRDS(here(sseep.analysis, "sdmtmb", "scup", "data","fall_grid_preds_tw_2.rds")) #based on filtered mean depth.
 
 
-fall_preds_2 <- fall_preds_2 |>
-  dplyr::select(!c(median_1, mean_1, n1, Cell_Area, median_2,mean_2, n2, New_area, Perc_reduction, Perc_Reduction))
+fall_preds <- fall_preds |>
+  dplyr::select(!c(median_1, mean_1, n1, Cell_Area, median_2,mean_2, n2, New_area, Perc_reduction))
 
 source(file = here(sseep.analysis,"sdmtmb", "R", "plot_fns.R"))
-plot_preds(fall_preds_m1, exp(est)) + scale_fill_viridis_c(trans = "sqrt", option = "H") +
+plot_preds(fall_preds, exp(est)) + scale_fill_viridis_c(trans = "sqrt", option = "H") +
   labs(fill = "Biomass estimates") +
   theme(legend.position = "right")
 
@@ -148,7 +139,7 @@ plot_surface(pop, mat = "N")
 
 ## APPEND DISTRIBUTION ####
 
-preds_5yr_m1 <- fall_preds_m1 |>
+preds_5yr <- fall_preds |>
   filter(EST_YEAR %in% c(2010,2011,2012,2013,2014)) |>
   rename(year = EST_YEAR, #rename to match column calls in SimSurvey
          strat = STRATUM) |>
@@ -166,7 +157,7 @@ preds_5yr_m1 <- fall_preds_m1 |>
 
 
 #plot it
-ggplot(preds_5yr_m1) +
+ggplot(preds_5yr) +
   geom_tile(aes(X, Y, fill = N_dist), width = 10, height = 10) +
   scale_fill_viridis_c() +
   facet_wrap(~year) +
@@ -174,10 +165,10 @@ ggplot(preds_5yr_m1) +
   theme_bw() +
   theme(legend.position = "bottom")
 
-summary(preds_5yr_1$AVGDEPTH)
+summary(preds_5yr$AVGDEPTH)
 
 # predicted numbers at age based on simulated N0 and sdmtmb predictions
-dist_m2 <- sdmTMB::replicate_df(preds_5yr_m2, "age", c(0:7)) |> # replicate the predictions over each age, so there are distributions for each age
+dist <- sdmTMB::replicate_df(preds_5yr, "age", c(0:7)) |> # replicate the predictions over each age, so there are distributions for each age
   left_join(Nage, by = c("age","year")) |> # join populations at age and year to the predictions
   rename(x = X,
          y = Y,
@@ -190,10 +181,10 @@ dist_m2 <- sdmTMB::replicate_df(preds_5yr_m2, "age", c(0:7)) |> # replicate the 
          division = 1) |>
   dplyr::select(age, year, cell, N, x, y, strat, depth)
 
-summary(dist_2$depth)
+summary(dist$depth)
 
 # forced numbers at age outside sdmTMB prediction area
-no_dist_1 <- dplyr::anti_join(grid_xy_1, preds_5yr_1, by = "cell") |>
+no_dist <- dplyr::anti_join(grid_xy, preds_5yr, by = "cell") |>
   sdmTMB::replicate_df("age", c(0:7)) |>
   sdmTMB::replicate_df("year", c(1:5)) |>
   mutate(N = 0, #
@@ -201,35 +192,34 @@ no_dist_1 <- dplyr::anti_join(grid_xy_1, preds_5yr_1, by = "cell") |>
          cell = as.double(cell),
          division = 1)
 
-summary(no_dist_1$depth)
+summary(no_dist$depth)
 
 # bind distributions
-full_dist_1 <- bind_rows(no_dist_1, dist_1) |>
+full_dist <- bind_rows(no_dist, dist) |>
   as.data.table()
 
 
-full_dist_2 |>
+full_dist |>
   ggplot() +
   geom_tile(aes(x,y, fill = N), width = 10, height = 10) +
   scale_fill_viridis_c() + facet_wrap(~age)
 
 
-pop_2 <- pop
 # append  to abundance and rename list item
-pop_2[[10]] <- grid_stars_2 # use stars object of  grid
-names(pop_2)[[10]] <- "grid"
+pop[[10]] <- grid_stars # use stars object of  grid
+names(pop)[[10]] <- "grid"
 
 # append to the simulated abundance object
-pop_2[[11]] <- grid_xy_2
-names(pop_2)[[11]] <- "grid_xy"
+pop[[11]] <- grid_xy
+names(pop)[[11]] <- "grid_xy"
 
 # append to the simulated abundance object
-pop_2[[12]] <- full_dist_2
-names(pop_2)[[12]] <- "sp_N"
+pop[[12]] <- full_dist
+names(pop)[[12]] <- "sp_N"
 
 
 # plot numbers at age
-ggplot(pop_m2$sp_N) +
+ggplot(pop$sp_N) +
   geom_tile(aes(x, y, fill = N), width = 10, height = 10) +
   scale_fill_viridis_c(trans = "sqrt") +
   facet_wrap(~age, scales = "free_y") +
@@ -237,18 +227,11 @@ ggplot(pop_m2$sp_N) +
   theme_bw() +
   theme(legend.position = "bottom", legend.text = element_text(angle = 45))
 
-#saveRDS(pop_m2, here("R", "scup", "data", "forced-pop-dist_1.rds"))
-pop_1 <- readRDS(here("R", "scup", "data", "forced-pop-dist_1.rds"))
-pop_2 <- readRDS(here("R", "scup", "data", "forced-pop-dist_2.rds"))
-pop_m1 <- readRDS(here("R", "scup", "data", "forced-pop-dist_m1.rds"))
-pop_m2 <- readRDS(here("R", "scup", "data", "forced-pop-dist_m2.rds"))
+saveRDS(pop, here("R", "scup", "data", "forced-pop-dist.rds"))
+#pop <- readRDS(here("R", "scup", "data", "forced-pop-dist.rds"))
 
-summary(pop_1$sp_N$depth)
-summary(pop_2$sp_N$depth)
 
-summary(pop_m1$sp_N$depth)
-summary(pop_m2$sp_N$depth)
-
+summary(pop$sp_N$depth)
 
 #-----------------------------------------------------------------
 #function to set selectivity
@@ -257,7 +240,7 @@ source(here("R/selectivity_fns.R"))
 
 #-----------------------------------------------------------------
 ## SIMULATE SURVEY ####
-surv_dat_m2 <- pop_m2 |>
+surv_dat <- pop |>
   sim_survey(n_sims = 25,
              trawl_dim = c(2.7, 0.014),
              q = force_sim_logistic(k = -0.66, x0 = -1.14, plot = TRUE, force_age = TRUE, age = 0, force_sel = 1),
@@ -272,24 +255,22 @@ surv_dat_m2 <- pop_m2 |>
 
 
 
-plot_survey(surv_dat_m1, which_year = 1)
-plot_survey(surv_dat_m2, which_year = 1)
-# plot_survey(surv_dat, which_year = 2)
-# plot_survey(surv_dat, which_year = 3)
-# plot_survey(surv_dat, which_year = 4)
-# plot_survey(surv_dat, which_year = 5)
+plot_survey(surv_dat, which_year = 2)
+plot_survey(surv_dat, which_year = 3)
+plot_survey(surv_dat, which_year = 4)
+plot_survey(surv_dat, which_year = 5)
 
 
 
 # plot it
-data_m2 <- surv_dat_m2$setdet
+data <- surv_dat$setdet
 
-ggplot(data_m2, aes(x = sim, y = depth)) +
-  geom_point(color = "orange") + # Add points
-  labs(title = "Depth per Tow - Sim Survey",
-       x = "Tow ID",
-       y = "Depth (m)") +
-  theme_minimal()
+# ggplot(data, aes(x = sim, y = depth)) +
+#   geom_point(color = "orange") + # Add points
+#   labs(title = "Depth per Tow - Sim Survey",
+#        x = "Tow ID",
+#        y = "Depth (m)") +
+#   theme_minimal()
 
 wind_areas <- readRDS(here("R", "scup","data", "all_wind_areas_Jun2022.rds"))
 strata <- readRDS(here("R", "scup","data", "active_strata.rds"))
@@ -302,12 +283,12 @@ ggplot() +
   geom_sf(data = strata_utm, fill = NA) +
   geom_sf(data = wind_areas_utm, fill = "lightblue") +
   coord_sf() +
-  geom_point(data = data_m2, aes(x*1000,y*1000, size = n, color = n)) +
+  geom_point(data = data, aes(x*1000,y*1000, size = n, color = n)) +
   facet_wrap(~year) +
   scale_color_continuous(type = "viridis") +
   scale_size_continuous(guide = "none") +
   theme_bw() +
-  labs(x = "Longitude", y = "Latitude", color = "Number", title="Sim Catch Rate", subtitle="median filtered") +
+  labs(x = "Longitude", y = "Latitude", color = "Number", title="Sim Catch Rate", subtitle="mean filtered") +
   theme(legend.position = "right", panel.spacing =  unit(1, "lines"), axis.text.x = element_text(angle = 45, vjust = 0.5))
 
 
@@ -390,25 +371,25 @@ nsurveys <- 25
 ages <- 0:7
 years <- 1:5
 
-survdat_sq_1 <- readRDS(here(survdat, str_c(species, season, length(nsims), "sims", nsurveys, "sq-surv-dat_1.rds", sep = "_")))
+survdat_sq <- readRDS(here(survdat, str_c(species, season, length(nsims), "sims", nsurveys, "sq-surv-dat.rds", sep = "_")))
 
-survdat_sq_1 <- survdat_sq_1[[1]]
+survdat_sq <- survdat_sq[[1]]
 
-fixed_grid_1 <- survdat_sq_1$grid_xy
+fixed_grid <- survdat_sq$grid_xy
 
 # Access the grid from the pop object
-pop_grid_1 <- pop_1$grid_xy
+pop_grid <- pop$grid_xy
 
 # Subset the pop grid to include only the cells from the fixed grid
 # cell is the column that contains the grid cell IDs in both pop and fixed_locations
-grid_fixed_1 <- pop_grid_1[pop_grid_1$cell %in% fixed_grid_1$cell, ]
+grid_fixed <- pop_grid[pop_grid$cell %in% fixed_grid$cell, ]
 
 # Update pop object to use only the fixed grid cells
-pop_fixed_1 <- pop_1
-pop_fixed_1$grid_xy <- grid_fixed_1
+pop_fixed <- pop
+pop_fixed$grid_xy <- grid_fixed
 
 # survey with fixed locations will need a different minimum set parameter, without replacement
-surv_dat_fixed_m1 <- pop_fixed_m1 |>
+surv_dat_fixed <- pop_fixed |>
   sim_survey(n_sims = 100,
              trawl_dim = c(2.7, 0.014),
              q = force_sim_logistic(k = -0.66, x0 = -1.14, plot = TRUE, force_age = TRUE, age = 0, force_sel = 1),
@@ -420,43 +401,33 @@ surv_dat_fixed_m1 <- pop_fixed_m1 |>
 
 
 
-saveRDS(surv_dat_fixed_m1, here("R", "scup", "data", "surv_dat_fixed_m1.rds"))
-surv_dat_fixed_1<- readRDS(here("R", "scup","data", "surv_dat_fixed_m1.rds"))
-surv_dat_fixed_2<- readRDS(here("R", "scup","data", "surv_dat_fixed_m2.rds"))
-surv_dat_fixed_m1<- readRDS(here("R", "scup","data", "surv_dat_fixed_m1.rds"))
-surv_dat_fixed_m2<- readRDS(here("R", "scup","data", "surv_dat_fixed_m2.rds"))
-
+saveRDS(surv_dat_fixed, here("R", "scup", "data", "surv_dat_fixed.rds"))
+surv_dat_fixed<- readRDS(here("R", "scup","data", "surv_dat_fixed.rds"))
 
 
 # Extract sampled locations from surv_dat_fixed
-sampled_locations_1 <- surv_dat_fixed_1$grid$cell  # Modify based on the structure of surv_dat_fixed
+sampled_locations <- surv_dat_fixed$grid$cell  # Modify based on the structure of surv_dat_fixed
 
 
 #Data frame for fixed and sampled locations
-fixed_df_1 <- data.frame(x = fixed_grid_1$x, y = fixed_grid_1$y)
-sampled_df_1 <- data.frame(x = surv_dat_fixed_1$grid_xy$x, y = surv_dat_fixed_1$grid_xy$y)
+fixed_df <- data.frame(x = fixed_grid$x, y = fixed_grid$y)
+sampled_df <- data.frame(x = surv_dat_fixed$grid_xy$x, y = surv_dat_fixed$grid_xy$y)
 
 # Plot the fixed and sampled locations
 ggplot() +
-  geom_point(data = fixed_df_1, aes(x = x, y = y), color = "blue", size = 3, alpha = 0.5) +
-  geom_point(data = sampled_df_1, aes(x = x, y = y), color = "red", size = 2, alpha = 0.7) +
+  geom_point(data = fixed_df, aes(x = x, y = y), color = "blue", size = 3, alpha = 0.5) +
+  geom_point(data = sampled_df, aes(x = x, y = y), color = "red", size = 2, alpha = 0.7) +
   labs(title = "Fixed vs Sampled Locations",
        x = "Long", y = "Lat")
 
 
-surv_fixed_setdet_1 <- surv_dat_fixed_1$setdet
-surv_fixed_setdet_2 <- surv_dat_fixed_2$setdet
-surv_fixed_setdet_m1 <- surv_dat_fixed_m1$setdet
-surv_fixed_setdet_m2 <- surv_dat_fixed_m2$setdet
+surv_fixed_setdet <- surv_dat_fixed$setdet
 
-summary(surv_fixed_setdet_1$depth)
-summary(surv_fixed_setdet_2$depth)
-summary(surv_fixed_setdet_m1$depth)
-summary(surv_fixed_setdet_m2$depth)
+summary(surv_fixed_setdet$depth)
+
 
 # plot it
-
-#data2 <- surv_dat_fixed$setdet
+data2 <- surv_dat_fixed$setdet
 
 #simulated catch rate
 ggplot() +
@@ -479,6 +450,7 @@ fordepth <- data2 |>
          TYPE = "Simulated") |>
   dplyr::select(sim, TOWID, n, year, TYPE, depth, strat, cell, x, y) |>
   rename(replicate = sim)
+
 
 fordepth |> filter(depth >100) |>
 ggplot() +
@@ -523,7 +495,7 @@ test_data2 <- bind_rows(obs_catch, sim_data2)
 
 ### PROPORTION OF ZEROS ####
 # proportions of zeros in sim is very diff from proportion of zeros of real data - why is that?
-prop_zero <- test_data |>
+prop_zero <- test_data |> #test data is for random survey loc
   group_by(TYPE, replicate, year) |>
   nest() |>
   mutate(zero = map(data, ~filter(., n == 0) |> nrow()),
@@ -532,7 +504,7 @@ prop_zero <- test_data |>
   unnest(cols = c(zero, total)) |>
   summarise(prop = round((zero/total)*100, 0))
 
-prop_zero2 <- test_data2 |>
+prop_zero2 <- test_data2 |> #test_data2 is for fixed survey loc
   group_by(TYPE, replicate, year) |>
   nest() |>
   mutate(zero = map(data, ~filter(., n == 0) |> nrow()),
@@ -669,10 +641,3 @@ ggplot(test_data2) +
   geom_point(aes(x = depth, y = n)) +
   facet_wrap(~TYPE) +
   labs(x = "Depth (m)", y = "Catch rate (N/tow)", subtitle = "Distribution of catch rates with respect to depth - fixed loc")
-
-
-
-
-
-
-
